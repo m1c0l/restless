ba7bc99943055a61686cf6b933091d8cfedd2ff1
Vincent Jin <vincentjinir@ucla.edu>
2016-10-22 22:49:05 -0700

    added sqlalch for skill

---
+++ backend/skill.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database import db
+
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    skill_name = db.Column(db.String(64), unique=True)
