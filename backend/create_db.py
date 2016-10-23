300d6db716dbb688ad3624fae336ca9411b50453
Vincent Jin <vincentjinir@ucla.edu>
2016-10-22 23:48:31 -0700

    renaming

--- backend/create_db.py
+++ backend/create_db.py
@@ -2,7 +2,7 @@ from database import db
 from user import User
 from project import Project
 from skill import Skill
-
+from devs_to_projects import devs_to_projects
 
 db.drop_all()
 db.create_all()
