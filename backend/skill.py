509241029dfdae55bf8ca2f416d2f5049a0a59ad
Michael Li <michael.li11702@gmail.com>
2016-10-23 06:32:20 +0000

    Make user and project fields not null, quick fixes

--- backend/skill.py
+++ backend/skill.py
@@ -4,4 +4,5 @@ from database import db
 
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    skill_name = db.Column(db.String(64), unique=True)
+    skill_name = db.Column(db.String(64), unique=True, nullable=False)
+
