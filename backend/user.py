76e9c830e2fb1c76b8c09de873d9e45409f7fa2b
Richard Sun <richardsun29@ucla.edu>
2016-10-23 06:26:19 +0000

    add db constraints for user

--- backend/user.py
+++ backend/user.py
@@ -4,11 +4,12 @@ from database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.Text)
-    last_name = db.Column(db.Text)
-    email = db.Column(db.Text)
-    LinkedIn_profile_id = db.Column(db.Text)
-    bio = db.Column(db.Text)
-    signup_time = db.Column(db.DateTime)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    LinkedIn_profile_id = db.Column(db.Text, nullable=True)
+    bio = db.Column(db.Text, nullable=False, default="")
+    signup_time = db.Column(db.DateTime, nullable=False)
 
     #projects = db.relationship('Project', backref='user')
