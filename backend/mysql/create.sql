d079e134e8f033b4e75e24eb3990acd8bbfd0b7d
Michael Li <michael.li11702@gmail.com>
2016-10-23 05:16:24 +0000

    Basic DB setup for users, projects, and skills

--- backend/mysql/create.sql
+++ backend/mysql/create.sql
@@ -1,10 +1,28 @@
-# Movie
+-- Skills
 
-CREATE TABLE Movie (
-	id INT,
-	title VARCHAR(100),
-	year INT,
-	rating VARCHAR(10),
-	company VARCHAR(50),
-  PRIMARY KEY(id) # each Movie has a unique ID
-) ENGINE=INNODB;
+CREATE TABLE Skills (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT name_unique UNIQUE(name)
+);
+
+CREATE TABLE UsersInfo (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    LinkedIn_profile_id TEXT, -- can be null
+    bio TEXT,
+    signup_time TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE ProjectsInfo (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    current_state SMALLINT NOT NULL, -- recruiting, etc.
+    PRIMARY KEY(id),
+    CONSTRAINT title_unique UNIQUE(title)
+);
--- backend/mysql/load.sql
+++ backend/mysql/load.sql
@@ -1,4 +1,6 @@
 # Movie
 
-LOAD DATA LOCAL INFILE '~/data/movie.del' INTO TABLE Movie
-	FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+#LOAD DATA LOCAL INFILE '~/data/movie.del' INTO TABLE Movie
+	#FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+INSERT INTO UsersInfo
+VALUES(id=LAST_INSERT_ID(), first_name="John", last_name="Dough", email="blah", LinkedIn_profile_id=null, bio=null, signup_time=NOW());
