676987eb83d8a533fe7b647904d1db32e530f6aa
Richard Sun <richardsun29@ucla.edu>
2016-10-30 04:19:10 +0000

    return the error messages

--- backend/route.py
+++ backend/route.py
@@ -23,12 +23,12 @@ def error(msg='Bad Request', status='BAD_REQUEST', code=400): #todo: add logging
         
 @app.route("/")
 def index():
-    error(msg='There is no index!')
+    return error(msg='There is no index!')
 
 @app.route("/<type>/<id>/", methods=['GET', 'POST'])
 def retrieve(type,id): # todo: find stuff with the id
     if type == "" or id == "":
-        error(msg='Please fill in your parameters!')
+        return error(msg='Please fill in your parameters!')
     if type.lower() == 'user':
         pass
     elif type.lower() == 'project':
@@ -36,7 +36,7 @@ def retrieve(type,id): # todo: find stuff with the id
     elif type.lower() == 'skill':
         pass
     else:
-        error(msg='Invalid type.')
+        return error(msg='Invalid type.')
 
 if __name__ == "__main__":
     init_app()
