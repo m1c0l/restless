8823fb9daaff4a0bd112d0f0e38fc68f24b90190
Michael Li <michael.li11702@gmail.com>
2016-10-30 05:57:20 +0000

    Add primary key to swipe table, works now

--- backend/database/swipe.py
+++ backend/database/swipe.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from database import db
 
 class Swipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     result = db.Column(db.Integer, nullable=False)
