70c1ae212176527eaa0a1bc858bbdb1c2d397d80
Richard Sun <richardsun29@ucla.edu>
2016-10-30 07:56:28 +0000

    push context instead of "with" block

--- backend/database/test_insert.py
+++ backend/database/test_insert.py
@@ -15,23 +15,24 @@ from project_skills import project_skills
 from user_skills import user_skills
 from swipe import Swipe
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+app.app_context().push()
 
-    u = User(first_name="John", last_name="Dough", email="xyz", username="jd", bio="m4st3r h4x0r")
-    db.session.add(u)
-    db.session.commit()
+db.drop_all()
+db.create_all()
 
-    s = Skill(skill_name="Python")
-    db.session.add(s)
-    db.session.commit()
+u = User(first_name="John", last_name="Dough", email="xyz", username="jd", bio="m4st3r h4x0r")
+db.session.add(u)
+db.session.commit()
 
-    p = Project(title="H4cks", description="M4st3r h4cks 4 dayz", pm_id=u.id)
-    db.session.add(p)
-    db.session.commit() 
+s = Skill(skill_name="Python")
+db.session.add(s)
+db.session.commit()
 
-    sw = Swipe(user_id=u.id, project_id=p.id, who_swiped=0)
-    db.session.add(sw)
-    db.session.commit() 
+p = Project(title="H4cks", description="M4st3r h4cks 4 dayz", pm_id=u.id)
+db.session.add(p)
+db.session.commit() 
+
+sw = Swipe(user_id=u.id, project_id=p.id, who_swiped=0)
+db.session.add(sw)
+db.session.commit() 
 
