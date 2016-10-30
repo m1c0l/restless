61ac34f19f147df5464660c15bd56d9971181427
Michael Li <michael.li11702@gmail.com>
2016-10-30 08:21:05 +0000

    Add template function for inserting, add more users

--- backend/database/test_insert.py
+++ backend/database/test_insert.py
@@ -17,22 +17,28 @@ from swipe import Swipe
 
 app.app_context().push()
 
+def insert_db_obj(obj):
+    db.session.add(obj)
+    db.session.commit()
+
 db.drop_all()
 db.create_all()
 
-u = User(first_name="John", last_name="Dough", email="xyz", username="jd", bio="m4st3r h4x0r")
-db.session.add(u)
-db.session.commit()
+user_arr = [
+    User(first_name="John", last_name="Dough", email="xyz", username="jd", bio="m4st3r h4x0r"),
+    User(first_name="Mike", last_name="Li", email="mail", username="mic", bio="admin"),
+    User(first_name="Rich", last_name="Sun", email="rich", username="rich", bio="waffle the bunny"),
+    User(first_name="Vince", last_name="Jin", email="jinir", username="vince", bio="flask dev")
+]
+for u in user_arr:
+    insert_db_obj(u)
 
 s = Skill(skill_name="Python")
-db.session.add(s)
-db.session.commit()
+insert_db_obj(s)
 
-p = Project(title="H4cks", description="M4st3r h4cks 4 dayz", pm_id=u.id)
-db.session.add(p)
-db.session.commit() 
+p = Project(title="H4cks", description="M4st3r h4cks 4 dayz", pm_id=user_arr[0].id)
+insert_db_obj(p)
 
-sw = Swipe(user_id=u.id, project_id=p.id, who_swiped=0)
-db.session.add(sw)
-db.session.commit() 
+sw = Swipe(user_id=user_arr[0].id, project_id=p.id, who_swiped=0)
+insert_db_obj(sw)
 
