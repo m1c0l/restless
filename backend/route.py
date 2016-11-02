ada2ace693e8312d69d90d90c0892c4a902d96f7
Richard Sun <richardsun29@ucla.edu>
2016-11-02 03:09:14 +0000

    serve backend docs on server <ip>/docs/

--- backend/route.py
+++ backend/route.py
@@ -25,7 +25,7 @@ def error(msg='Bad Request', status='BAD_REQUEST', code=400): #todo: add logging
 def index():
     return error(msg='There is no index!')
 
-@app.route("/<type>/<id>/", methods=['GET', 'POST'])
+@app.route("/api/<type>/<id>/", methods=['GET', 'POST'])
 def retrieve(type,id): # todo: find stuff with the id
     if type == "" or id == "":
         return error(msg='Please fill in your parameters!')
@@ -37,6 +37,14 @@ def retrieve(type,id): # todo: find stuff with the id
         pass
     else:
         return error(msg='Invalid type.')
+
+@app.route("/docs/")
+def docs_index():
+    return flask.send_from_directory(app.config['DOCS_PATH'], 'index.html')
+
+@app.route("/docs/<path:filename>")
+def serve_docs(filename):
+    return flask.send_from_directory(app.config['DOCS_PATH'], filename)
 
 if __name__ == "__main__":
     init_app()
