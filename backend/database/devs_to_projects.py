807a655b5b78036ddda6d03274ac2487b1fcf2a7
Michael Li <michael.li11702@gmail.com>
2016-11-03 08:11:27 +0000

    Add comments to helper table variables

--- backend/database/devs_to_projects.py
+++ backend/database/devs_to_projects.py
@@ -1,5 +1,9 @@
 from db import db
 
+"""
+This creates a helper table that maps the many-to-many dependencies between
+developers and the projects they're part of.
+"""
 devs_to_projects = db.Table('devs_to_projects',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('project_id', db.Integer, db.ForeignKey('project.id'))
