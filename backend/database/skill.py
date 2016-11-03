c193ecb72b8d5cd0a7b15b2d027ae725818782de
Michael Li <michael.li11702@gmail.com>
2016-11-03 05:09:08 +0000

    Add docs for skill class

--- backend/database/skill.py
+++ backend/database/skill.py
@@ -1,10 +1,27 @@
 from db import db
 
 class Skill(db.Model):
+    """
+    This class is the database model for the skill sets that a L{User} 
+    can have and are used in a L{Project}.
+    """
     id = db.Column(db.Integer, primary_key=True)
+    """
+    A Skill's id. This is Skill's primary key in the database.
+    """
     skill_name = db.Column(db.String(64), unique=True, nullable=False)
+    """
+    The name of the skill.
+    @type: C{str}
+    """
 
     def __init__(self, skill_name):
+        """
+        Construct a Skill
+
+        @param skill_name: The name of the skill being constructed
+        @type skill_name: C{str}
+        """
         self.skill_name = skill_name
 
     def __repr__(self):
