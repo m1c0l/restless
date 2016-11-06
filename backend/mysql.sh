e63947e3845d70e363d5846e21e92ed966046931
Richard Sun <richardsun29@ucla.edu>
2016-11-06 08:50:46 +0000

    mysql.sh reads database info from config.py
    This lets you use the script even if you change the database in config.py.

--- backend/mysql.sh
+++ backend/mysql.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-db=restless
-user=restless
-pass=r3stl355
+
+config='config.py'
+db_info=($(grep 'SQLALCHEMY_DATABASE_URI' $(dirname ${BASH_SOURCE[0]})/$config |
+          sed 's;.*mysql://;;' |
+          awk -F "[:@/']" '{print $1; print $2; print $4;}'))
+
+user=${db_info[0]}
+pass=${db_info[1]}
+db=${db_info[2]}
+
 mysql $db -u $user -p$pass
